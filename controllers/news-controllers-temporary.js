660ee06021560263855e3e1160f6c2fd3cba291b
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2023-05-14 18:01:02 +0300

    add cost

--- controllers/news-controllers-temporary.js
+++ controllers/news-controllers-temporary.js
@@ -1,15 +1,17 @@
 const { ctrlWrapper } = require("../utils");
 const { News } = require("../models/news-temporary");
-const { HttpError } = require("../helpers");
+// const { HttpError } = require("../helpers");
 
 const allNews = async (req, res) => {
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await News.find({}, null, {
+  const hits = await News.find({}, null, {
     skip,
     limit,
   }).sort({ date: -1 });
-  res.json(result);
+
+  const totalHits = await News.count();
+  res.json({ totalHits, hits });
 };
 
 module.exports = {
